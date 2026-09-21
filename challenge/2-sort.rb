ec81652188b43a00cafc6bb6e87a4d00f97e760c
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-21 23:28:44 +0400

    m

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,23 +1,28 @@
-#!/usr/bin/node
-/*
-    Print a square with the character #
+#!/usr/bin/ruby
+# Sort integer arguments (ascending)
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
     
-    The size of the square must be the first argument 
-    of the program.
-*/
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg) # Fixed: changed i - 1 to i
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
-}
-
-const size = parseInt(process.argv[2], 10);
-
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
-}
+puts result
