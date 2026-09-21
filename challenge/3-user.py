d4a058a28fcf1bc870afcc3be50dc43510110b4f
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-21 23:37:10 +0400

    m

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -40,7 +40,6 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            # Fixed: changed self._password to self.__password
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
@@ -55,7 +54,6 @@ class User():
             return False
         if self.__password is None:
             return False
-        # Fixed: changed .upper() to .lower() to match the setter
         return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
